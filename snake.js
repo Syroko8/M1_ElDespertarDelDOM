1536552ceb941679700a2630ca66dfb18487f3d0
Syroko8 <nicolaspueblamartin@gmail.com>
2026-09-23 11:30:31 +0200

    Implemented functionaligy and logic functions for movement controll and game flow, first minimally functional game commit.

--- snake.js
+++ snake.js
@@ -2,15 +2,21 @@
 const boardSize = 15;
 const chain = []
 const cells = [];
-const energy = {x:0, y:0};
+let energy = {x:0, y:0};
+let direction = 'right';
+let score = 0;
+let best = 0;
+let running = false;
+let speed = 200;
+let interval = null;
 
 // Elementos del DOM.
 const board = document.getElementById('board');
-
+const scoreCounter = document.getElementById('score');
 
 const directions = {
-    'up':   {x: 0, y: 1},
-    'down': {x: 0, y: -1},
+    'up':   {x: 0, y: -1},
+    'down': {x: 0, y: 1},
     'left': {x: -1, y: 0},
     'right': {x: 1, y: 0}
 }
@@ -23,7 +29,6 @@ const keys = {
 }
 
 const generateCells = () => {
-
     for (let i = 0; i < boardSize*boardSize; i++) {
         const newCell = document.createElement('div');
         newCell.className = 'cell';
@@ -36,14 +41,14 @@ const draw = () => {
     // Limpiamos celdas.
     cells.forEach(cell => {
         cell.className = 'cell';
+        cell.style.opacity = 1;
     });
 
-    const newEnergy = generateEnergy();
-
-    // Cambiar estilo de la celda con nueva energía.
-
+    // Colocamos la enegía en el tablero.
+    placeEnergy();
 
     // Pintar cadena de energía.
+    drawChain();
 }
 
 /**
@@ -52,30 +57,136 @@ const draw = () => {
 const generateEnergy = () => {
     let newX, newY;
     do {
-        newX = Math.random() * 15;
-        newY = Math.random() * 15;
-    } while (isNotOnChain());
+        newX = Math.floor(Math.random() * boardSize);
+        newY = Math.floor(Math.random() * boardSize);
+    } while (isOnChain(newX, newY));
     energy.x = newX;
     energy.y = newY;
 }
 
 /**
- * Comprueba que la nueva energía generada no está sobre la cadena.
+ * Comprueba que las coordenadas recibidas no estén sobre la cadena.
  */
-const isNotOnChain = (x, y) => {
-    chain.forEach(cell => {
-        x === cell.x && y === cell.y ? false : true;
-    });
-    return true;
+const isOnChain = (x, y) => {
+    return chain.some(cell => cell.x === x && cell.y === y);
 }
+
+/**
+ * Añade la casilla con el nuevo orbe al tablero.
+ */
+const placeEnergy = () => {
+    const energyCell = cellAt(energy.x, energy.y);
+    energyCell.classList.add('energy');
+} 
 
 /**
  * Devuelve el elemento del array de celdas según la posición en coordenadas.
  */
 const cellAt = (x, y) => {
-    return cells[y * size + x];
+    return cells[y * boardSize + x];
 }
 
+/**
+ * Colóca la cadena de orbes en sus celdas.
+ */
+const drawChain = () => {
+    const head = cellAt(chain[0].x, chain[0].y);
+    head.classList.add('head');
+
+    let i = 0;
+    chain.forEach(bodyOrb => {
+        const bodyCell = cellAt(bodyOrb.x, bodyOrb.y);
+        /* Disminuimos la opacidad según retrocedamos, con una mínuma de 0.3.
+        aumentando 0.1 cada vez.*/
+        bodyCell.classList.add('chain');
+        bodyCell.style.opacity = Math.max(0.3, 1 - i * 0.1);
+        i++;
+    });
+}
+
+/**
+ * Efectúa un ciclo del juego, un movimiento de la cadena.
+ */
+const step = () => {
+    // Nueva posición de la cabeza.
+    const head = {
+        x: chain[0].x + directions[direction].x,
+        y: chain[0].y + directions[direction].y
+    }
+
+    // Comprobamos que la cadena no esté tocando un borde, ni a sí misma.
+    const hittingWall = head.x < 0 || head.x >= boardSize ||
+     head.y < 0 || head.y >= boardSize;
+    if (hittingWall || isOnChain(head.x, head.y)) {
+        // Game over.
+        return;
+    }
+
+    // Añadimos la cabeza al inicio de la lista.
+    chain.unshift(head);
+
+    // Si no ha comido, quitamos el último elemento.
+    if (head.x != energy.x || head.y != energy.y) {
+        chain.pop();
+    } else {
+        // Si ha comido aumentamos el contador y no retiramos el último elemento.
+        score++;
+        scoreCounter.textContent = `SCORE ${score}`;
+        // Generamos un nuevo orbe.
+        generateEnergy();
+    }
+    draw();
+}
+
+const endGame = () => {
+    
+    // clearInterval(interval);
+}
+
+const startGame = () => {
+    // Generamos celdas.
+    generateCells();
+    // Generamos cadena de inicial.
+    chain.push({x: 7, y: 7});
+    chain.push({x: 6, y: 7});
+    // Iniciamos juego.
+    running = !running;
+    interval = setInterval(step, speed);
+}
+
+const resetGame = () => {
+
+}
+
+/**
+ * Función que cambia la dirección en la que se mueve la cadena, evitando 
+ * que haga un giro de 180º.
+ */
+const changeDirection = (newDirection) => {
+    // Si no estamos jugando, no haremos nada.
+    if (!running) return; 
+
+    // Comprobamos que no se esté intentando dar la vuelta.
+    const newDirectionCoord = directions[newDirection];
+    const actualDirection = directions[direction];
+    if (actualDirection.x === -newDirectionCoord.x && actualDirection.y === -newDirectionCoord.y) return;
+
+    direction = newDirection;
+}
+
+/**
+ * Event listener que captura teclas para afectar el movimiento de la cadena.
+ */
+document.addEventListener("keydown", (event) => {
+    const key = event.key.toLowerCase();
+    // Comprobamos que la tecla esté relacionada con un cambio de sentido.
+    if (keys[key] != undefined) {
+        /* Evitamos que la web haga scroll al usar las teclas (No debería ocurrir ya que la 
+        web no tiene scroll).*/
+        event.preventDefault();
+        changeDirection(keys[key]); 
+    } 
+})
 
 /**Inicio del programa*/
-generateCells();
+startGame();
